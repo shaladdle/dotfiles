e4b25b0187fa6f335f09dcf3788ea71e165a9a08
Adam Wright <shaladdle@gmail.com>
2016-10-08 06:40:02 -0400

    Clean things up a bit

---
+++ shell-configs/aliases.sh
@@ -0,0 +1,17 @@
+# tmux
+alias tn='tmux new-s -s $(basename $(pwd))'
+alias ta='tmux atta -t'
+alias tl='tmux list-s'
+alias vim='mvim -v'
+
+# git
+alias gs='git status'
+alias gl='git lg'
+
+# rust
+alias cb='cargo build'
+
+# vim
+alias v='vim'
+
+EDITOR="mvim -v"
